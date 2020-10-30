56f515e70322505c2c83a1006a564cee6a199d45
officiallynik <nikhilownstheworld@gmail.com>
2020-10-30 13:39:45 +0530

    added progress bar component

---
+++ components/linearprogress/index.tsx
@@ -0,0 +1,12 @@
+import { LinearProgress } from '@material-ui/core';
+import React from 'react';
+
+const CustomLinearProgress = () => {
+    return(
+        <div style={{paddingTop:"45px"}}>
+            <LinearProgress />
+        </div>
+    );
+};
+
+export default CustomLinearProgress;
