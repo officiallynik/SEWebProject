6a760ae245e8145af1172c78f07d5b92bbf1a527
officiallynik <nikhilownstheworld@gmail.com>
2020-11-30 20:52:27 +0530

    added messages to features that are not deliverable for r2

--- pages/insurance-loan/index.tsx
+++ pages/insurance-loan/index.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 
 const Blogs = () => {
     return (
-        <div style={{height: "calc(100vh - 45px)", display: "flex", justifyContent: "center", alignItems: "center"}}>
+        <div style={{height: "calc(100vh - 45px)", display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column"}}>
+            <div style={{padding: "50px", fontSize: "16px"}}>
+                Different loan/insurance schemes require different eligiblity and most of the time a physical presence, we are working on a generalized
+                solution to make the process easy. We will add this feature if the project is continued. Thanks for your patience!!!
+            </div>
             <img style={{width: "30%"}} src="/coming_soon.jpg" alt="coming soon" />
         </div>
     );
